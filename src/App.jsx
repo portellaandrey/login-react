4c13f8d1ce36228764ad4b522c1d258fb71688a4
Andrey Portella <andreyportella.parana@gmail.com>
2026-01-20 22:51:38 -0300

    fix: correct component import case for Linux build

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import "./App.css";
 import Login from "./components/login/Login";
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <Login />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
