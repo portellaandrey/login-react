609f1e0caa21a2249fcea29635437f92a4717bc5
Andrey Portella <andreyportella.parana@gmail.com>
2026-01-20 22:51:05 -0300

    fix: correct login component import path case

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
-import Login from "./components/Login/Login";
+import Login from "./components/login/Login";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Login />
     </div>
   )
